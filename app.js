ef8667698192190a7ce814f38b732fe498a710c1
thelonehegelian <thelonehegelian@gmail.com>
2020-07-01 03:33:10 +0100

    Get clean pathname

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // require http module
 const http = require('http');
+const url = require('url');
 
 // use http to create a server
 const server = http.createServer((req, res) => {
+	// get pathname
+	let parsedUrl = url.parse(req.url, true); // true calls the queryString method
+
+	let path = parsedUrl.pathname;
+	// clean the path
+	let cleanPath = path.replace(/^\/+|\/+$/g, '');
 	res.end(`Here's Johnny`);
 });
 
